3b354e6427ce21ac5baf9c63542f84f0eccd6b77
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2020-07-23 23:02:09 +0600

    first commit

---
+++ shopping-cart/event_hadler.js
@@ -0,0 +1,136 @@
+const btnMinus = document.getElementById("btnMinus");
+btnMinus.addEventListener('click',function()
+{   
+    const Unit = decreaseAmount("mobileAmount");
+   balanceMobile("mobilePrice",Unit);
+
+   const subTotal = document.getElementById("subTotal").innerText;
+const subTotalNumber = parseFloat(subTotal);
+const TotalBalance = subTotalNumber-1219;
+document.getElementById("subTotal").innerText = TotalBalance;
+
+TaxAmount();
+TotalAmount();
+})
+
+
+
+const btnPlus = document.getElementById("btnPlus");
+btnPlus.addEventListener('click',function()
+{   
+     const Unit = increaseAmount("mobileAmount");
+     balanceMobile("mobilePrice",Unit);
+
+     const subTotal = document.getElementById("subTotal").innerText;
+    const subTotalNumber = parseFloat(subTotal);
+    const TotalBalance = subTotalNumber+1219;
+    document.getElementById("subTotal").innerText = TotalBalance;
+
+    TaxAmount();
+    TotalAmount();
+})
+
+
+
+const btnMinusCasing = document.getElementById("btnMinusCasing");
+btnMinusCasing.addEventListener('click',function()
+{   
+const Unit = decreaseAmount("casingAmount");
+balanceCasing("casingPrice",Unit);
+
+const subTotal = document.getElementById("subTotal").innerText;
+const subTotalNumber = parseFloat(subTotal);
+const TotalBalance = subTotalNumber-59;
+document.getElementById("subTotal").innerText = TotalBalance;
+
+TaxAmount();
+TotalAmount();
+})
+
+
+
+const btnPlusCasing = document.getElementById("btnPlusCasing");
+btnPlusCasing.addEventListener('click',function()
+       {   
+            const Unit = increaseAmount("casingAmount");
+            balanceCasing("casingPrice",Unit);
+
+            const subTotal = document.getElementById("subTotal").innerText;
+            const subTotalNumber = parseFloat(subTotal);
+            const TotalBalance = subTotalNumber+59;
+            document.getElementById("subTotal").innerText = TotalBalance;
+
+            TaxAmount();
+            TotalAmount();
+
+       })
+
+
+function decreaseAmount(id)
+       {
+        const current = document.getElementById(id).value;
+        console.log(current);
+           const currentNumber = parseInt(current);
+           console.log(currentNumber);
+           const
+           totalAmount = currentNumber - 1;
+           document.getElementById(id).value = totalAmount;
+           return totalAmount;
+       }
+
+       function increaseAmount(id)
+       {
+        const current = document.getElementById(id).value;
+           const currentNumber = parseInt(current);
+           const
+           totalAmount = currentNumber + 1;
+           document.getElementById(id).value = totalAmount;
+           return totalAmount;
+       }
+       function balanceMobile(id,unit)
+       {
+        const unitPrice = document.getElementById(id).innerText;
+        const unitPriceNumber = 1219;
+        const balance = unit*unitPriceNumber;
+        document.getElementById(id).innerText = balance;
+        return balance;
+       }
+       function balanceCasing(id,unit)
+       {
+        const unitPriceNumber = 59;
+        const balance = unit*unitPriceNumber;
+        document.getElementById(id).innerText = balance;
+        return balance;
+       }
+       
+       function TaxAmount()
+       {
+           const TaxMobile = document.getElementById("mobileAmount").value;
+           const TaxCasing = document.getElementById("casingAmount").value;
+           
+           
+           const TaxMobileNumber = parseInt(TaxMobile);
+           const TaxCasingNumber = parseInt(TaxCasing);
+
+           const TotalTax = (TaxMobileNumber*62.5)+(TaxCasingNumber*3.7);
+
+           document.getElementById("Tax").innerText = TotalTax;
+       }
+
+       function TotalAmount()
+       {
+           const subTotal = document.getElementById("subTotal").innerText;
+
+           const Tax = document.getElementById("Tax").innerText;
+
+           const subTotalNumber = parseFloat(subTotal);
+           const TaxNumber = parseFloat(Tax);
+
+           const TotalPrice = subTotalNumber + TaxNumber;
+
+           document.getElementById("priceInTotal").innerText = TotalPrice;
+       }
+
+
+
+
